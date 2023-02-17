d9eb4e5d7f6cc2de82ed35868e7e49023680c682
Lasse Antonsen <31047935+lazerlasse@users.noreply.github.com>
2023-02-17 19:06:42 +0100

    Added privacy policy page to the api server for the apps privacy requirements.

--- LocalEyesWebAPI/Controllers/HomeController.cs
+++ LocalEyesWebAPI/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using LocalEyesWebAPI.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -13,6 +14,7 @@ namespace LocalEyesWebAPI.Controllers
             _logger = logger;
         }
 
+        [AllowAnonymous]
         public IActionResult Privacy()
         {
             return View();
